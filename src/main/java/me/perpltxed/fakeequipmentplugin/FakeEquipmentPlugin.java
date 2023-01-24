7dfdecea364fde2baa034bbcc8bbfec5c5d3c95a
M8E-ya <suryanshu02052000@gmail.com>
2023-01-24 20:35:28 +0530

    did it work

---
+++ src/main/java/me/perpltxed/fakeequipmentplugin/FakeEquipmentPlugin.java
@@ -0,0 +1,31 @@
+package me.perpltxed.fakeequipmentplugin;
+
+import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FakeEquipmentPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        new FakeEquipment(this) {
+            @Override
+            protected boolean onEquipmentSending(EquipmentSendingEvent equipmentEvent) {
+                if (equipmentEvent.getSlot() == EquipmentSlot.HELD) {
+                    equipmentEvent.setEquipment(new ItemStack(Material.DIAMOND));
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            protected void onEntitySpawn(Player client, LivingEntity visibleEntity) {
+                // Remember to change this if you're intercepting a different slot!
+                if (EquipmentSlot.HELD.isEmpty(visibleEntity)) {
+                    updateSlot(client, visibleEntity, EquipmentSlot.HELD);
+                }
+            }
+        };
+    }
+}
